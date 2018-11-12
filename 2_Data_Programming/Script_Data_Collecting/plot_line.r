7fe111ffd9a360139d2bc593ebb5d5b23c558215
daehyeop.ko <daehyeop.ko@gmail.com>
2018-11-13 00:35:41 +0900

    [R] ggplot line graph code practice

--- 2_Data_Programming/Script_Data_Collecting/plot_line.r
+++ 2_Data_Programming/Script_Data_Collecting/plot_line.r
@@ -1,43 +1,30 @@
-data <- c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,22,24,26,28,30,32,34,36,38,40)
-quantile(data)
-min(data)
+#install.packages("ggplot2")
+library("ggplot2")
+library("scales")
 
-setwd("c:/Users/daehyeop.ko/Documents/R")
-dataset <- read.csv(file="search_service_sample.csv", header=TRUE, sep=",")
+#setwd("c:/Users/daehyeop.ko/Documents/R")
+#dataset <- read.csv(file="search_service_sample.csv", header=TRUE, sep=",")
+dataset <- read.csv(file="C:/Users/XNOTE/dev/Study-Note/999_samples_for_testing/search_service_sample.csv", header=TRUE, sep=",")
 
-dataset$date
-mean(dataset$median)
-dataset[2]
+#change data type of 'date' column to Date 
+dataset$date <- as.Date(dataset$date)
 
-xrange <- seq(dataset$date[1], dataset$date[nrow(dataset)])
-yrange <- seq(min(dataset$median), max(dataset$median))
+#drawing ggplot
+graph <- ggplot(data=dataset, aes(x=date)) + 
+  geom_line(aes(y=max, colour = "max")) + 
+  geom_point(aes(y=max, colour="max"), size=3) +
+  geom_line(aes(y=X90percentile, colour = "90percentile")) +
+  geom_point(aes(y=X90percentile, colour="90percentile"), size=3) +
 
-plot(dataset$date, dataset$median, type="line")
-# convert factor to numeric for convenience 
-Orange$Tree <- as.numeric(Orange$Tree) 
-ntrees <- max(Orange$Tree)
+#set details of x and y scale
+graph +
+  scale_x_date(breaks = date_breaks("1 day"), labels = date_format("%m/%d")) +
+  scale_y_continuous(breaks = seq(0, max(dataset$max)+100, 100), minor_breaks = waiver()) +
 
-# get the range for the x and y axis 
-xrange <- range(dataset$date) 
-yrange <- range(dataset$median) 
-
-# set up the plot 
-plot(xrange, yrange, type="n", xlab="Age (days)",
-     ylab="Circumference (mm)" ) 
-colors <- rainbow(ntrees) 
-linetype <- c(1:ntrees) 
-plotchar <- seq(18,18+ntrees,1)
-
-# add lines 
-for (i in 1:ntrees) { 
-  tree <- subset(Orange, Tree==i) 
-  lines(tree$age, tree$circumference, type="b", lwd=1.5,
-        lty=linetype[i], col=colors[i], pch=plotchar[i]) 
-} 
-
-# add a title and subtitle 
-title("Tree Growth", "example of line plot")
-
-# add a legend 
-legend(xrange[1], yrange[2], 1:ntrees, cex=0.8, col=colors,
-       pch=plotchar, lty=linetype, title="Tree")
+#name graph
+graph + 
+  ggtitle("Search Service Performance") +
+  xlab("DATE") +
+  ylab("Processing time(ms)") + 
+  theme_bw()
+  
