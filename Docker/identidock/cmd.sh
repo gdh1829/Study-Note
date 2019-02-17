f9c063a1cb5b3d1a5ff2e23867237954f9810e33
Daehyeop Ko <daehyeop.ko@gmail.com>
2019-02-17 22:24:33 +0900

    [Docker] Dockerfile practice: added script to optionally run dev or prod environment

---
+++ Docker/identidock/cmd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -ex
+
+if [ "$ENV" == "PROD" ]; then
+	echo "Running Production Server"
+	exec uwsgi --http 0.0.0.0:9090 --wsgi-file /usr/src/app/identidock.py --callable app --stats 0.0.0.0:9191
+else
+	echo "Running Development Server"
+	exec python "identidock.py"
+fi
