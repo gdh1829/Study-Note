e0e3374944c4dd1677bfe8111bfb41c89cb08cfe
Daehyeop Ko <daehyeop.ko@gmail.com>
2019-03-10 18:24:41 +0900

    [Docker] fixed bash code convention

--- Docker/identidock/cmd.sh
+++ Docker/identidock/cmd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-if [ "$ENV" == "PROD" ]; then
+if [ "${ENV}" == "PROD" ]; then
 	echo "Running Production Server"
 	exec uwsgi --http 0.0.0.0:9090 --wsgi-file /usr/src/app/identidock.py --callable app --stats 0.0.0.0:9191
 else
