838ce04337acb1e7c5583f4792717fe3c26fcfc3
daehyeop.ko <daehyeop.ko@gmail.com>
2023-03-12 17:55:17 +0900

    Programmers: sum of sequence. improved solution

--- Algorithm/codingtest/src/test/kotlin/robin/codingtest/codingtest/SumOfSequence.kt
+++ Algorithm/codingtest/src/test/kotlin/robin/codingtest/codingtest/SumOfSequence.kt
@@ -37,4 +37,27 @@ class SumOfSequence {
         Assertions.assertThat(solution(3, -3)).isEqualTo(intArrayOf(-2, -1, 0))
         Assertions.assertThat(solution(3, 21)).isEqualTo(intArrayOf(6, 7, 8))
     }
+
+    /**
+     * 등차수열의 합 공식 이용.
+     * S(n) = n(a(1) + a(n)) / 2
+     * 연속된 수의 합을 요구하므로 증분을 1을 d라고 하면 시작점을 구할 수 있음.
+     * S(n) = n(a + a + (n-1)d) / 2
+     */
+    fun solution_improved(num: Int, total: Int): IntArray =
+        (total / num - (num - 1) / 2).let { t ->
+            println(t)
+            IntArray(num) { index -> index + t }.also { println(it.toList()) }
+        }
+
+    @Test
+    fun execute_solution_improved() {
+        Assertions.assertThat(solution_improved(3, 12)).isEqualTo(intArrayOf(3, 4, 5))
+        Assertions.assertThat(solution_improved(5, 15)).isEqualTo(intArrayOf(1, 2, 3, 4, 5))
+        Assertions.assertThat(solution_improved(4, 14)).isEqualTo(intArrayOf(2, 3, 4, 5))
+        Assertions.assertThat(solution_improved(5, 5)).isEqualTo(intArrayOf(-1, 0, 1, 2, 3))
+        Assertions.assertThat(solution_improved(1, 10)).isEqualTo(intArrayOf(10))
+        Assertions.assertThat(solution_improved(3, -3)).isEqualTo(intArrayOf(-2, -1, 0))
+        Assertions.assertThat(solution_improved(3, 21)).isEqualTo(intArrayOf(6, 7, 8))
+    }
 }
